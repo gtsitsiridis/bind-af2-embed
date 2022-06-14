a7a670754241bc09965a1bd3520c42ff17bec4e0
George Tsitsiridis <gtsitsiridis@gmail.com>
2022-06-14 23:04:49 +0200

    modified dataset structure

--- src/main.py
+++ src/main.py
@@ -4,11 +4,6 @@ from config import AppConfig
 
 def __main():
     dataset = Dataset(AppConfig())
-    ids = dataset.get_ids()
-    fold_array = dataset.get_fold_array()
-    embeddings = dataset.get_embeddings()
-    sequences = dataset.get_sequences()
-    labels = dataset.get_labels()
     pass
 
 
