c0b92c0e2c6d765ccc274ee80c403941de727ab8
George Tsitsiridis <gtsitsiridis@gmail.com>
2022-07-07 13:23:12 +0200

    added data scrits

---
+++ data/output/ml/clear.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -r stats/2022*
+rm -r models/2022*
+rm -r predictions/2022*
+
---
+++ data/output/ml/save_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp -r stats/$1 stats/$2
+cp -r models/$1 models/$2
+cp -r predictions/$1 predictions/$2
